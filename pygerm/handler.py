c2152d60cbf97cd8f3b4a75395c096db1d9f0a60
Julien Lhermitte <jrmlhermitte@gmail.com>
2017-11-14 16:12:09 -0500

    WIP : First attempt

--- pygerm/handler.py
+++ pygerm/handler.py
@@ -1,5 +1,11 @@
 from filestore import HandlerBase
 import h5py
+import numpy as np
+
+from .zmq import parsed_event_payload, DATA_TYPES
+
+# build a lookup table of the data types listed in zmq.py
+DATA_TYPEMAP = {name: num for num, name in enumerate(list(DATA_TYPES))}
 
 
 class GeRMHandler(HandlerBase):
@@ -11,6 +17,21 @@ class GeRMHandler(HandlerBase):
 
     def __call__(self, column):
         return self._g[column][:]
+
+    def close(self):
+        self._file.close()
+
+
+class BinaryGeRMHandler(HandlerBase):
+    specs = {'BinaryGeRM'}
+
+    def __init__(self, fpath):
+        # TODO : don't save the raw data (here for debugging)
+        self.raw_data = np.fromfile(fpath, count=self.dlen, dtype=np.uint64)
+        self.data = parsed_event_payload(self.raw_data)
+
+    def __call__(self, column):
+        return self.data[DATA_TYPEMAP[column]]
 
     def close(self):
         self._file.close()
