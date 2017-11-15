3d63c03622d34b38f71a4eb54bc16f1ae3dcc525
Julien Lhermitte <jrmlhermitte@gmail.com>
2017-11-15 14:19:22 -0500

    added gitignore, check for endianness

--- pygerm/handler.py
+++ pygerm/handler.py
@@ -24,10 +24,24 @@ class BinaryGeRMHandler(HandlerBase):
 
     def __init__(self, fpath):
         # TODO : don't save the raw data (here for debugging)
-        self.raw_data = np.fromfile(fpath, count=self.dlen, dtype=np.uint64)
-        # remove first and last bit
-        self.raw_data = self.raw_data[1:-1]
-        self.data = parsed_event_payload(self.raw_data)
+        raw_data = np.fromfile(fpath, count=self.dlen, dtype=np.uint64)
+        # TODO : when simulated data comes in, verify this is correct
+        # endianness and correct for it, don't just raise error
+        first_word = raw_data[0] & 0xffffffff00000000
+        if first_word != 0xfeedface:
+            msg = "Error, first 32 bit word not 0xfeedface"
+            msg += f"\n Got {first_word:#x} instead"
+            raise ValueError(msg)
+
+        last_word = raw_data[-1] & 0xffffffff
+        if last_word != 0xdecafbad
+            msg = "Error, first 32 bit word not 0xdecafbad"
+            msg += f"\n Got {last_word:#x} instead"
+            raise ValueError(msg)
+
+        # remove first and last region
+        raw_data = raw_data[1:-1]
+        self.data = parsed_event_payload(raw_data)
 
     def __call__(self, column):
         return self.data[DATA_TYPEMAP[column]]
