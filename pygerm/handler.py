bdd141b220b0f7b66d5bdeae97c4c9bf117bfba8
Julien Lhermitte <jrmlhermitte@gmail.com>
2017-11-15 14:52:46 -0500

    TST: added test, need to clean up later

--- pygerm/handler.py
+++ pygerm/handler.py
@@ -2,7 +2,7 @@ from filestore import HandlerBase
 import h5py
 import numpy as np
 
-from .zmq import parsed_event_payload, DATA_TYPEMAP
+from .zmq import parse_event_payload, DATA_TYPEMAP
 
 
 class GeRMHandler(HandlerBase):
@@ -24,24 +24,24 @@ class BinaryGeRMHandler(HandlerBase):
 
     def __init__(self, fpath):
         # TODO : don't save the raw data (here for debugging)
-        raw_data = np.fromfile(fpath, count=self.dlen, dtype=np.uint64)
+        raw_data = np.fromfile(fpath, dtype=np.uint64)
         # TODO : when simulated data comes in, verify this is correct
         # endianness and correct for it, don't just raise error
-        first_word = raw_data[0] & 0xffffffff00000000
-        if first_word != 0xfeedface:
+        first_word = raw_data[0] & np.uint64(0xffffffff00000000)
+        if first_word != 0xfeedface00000000:
             msg = "Error, first 32 bit word not 0xfeedface"
             msg += f"\n Got {first_word:#x} instead"
             raise ValueError(msg)
 
-        last_word = raw_data[-1] & 0xffffffff
-        if last_word != 0xdecafbad
+        last_word = raw_data[-1] & np.uint64(0xffffffff)
+        if last_word != 0xdecafbad:
             msg = "Error, first 32 bit word not 0xdecafbad"
             msg += f"\n Got {last_word:#x} instead"
             raise ValueError(msg)
 
         # remove first and last region
         raw_data = raw_data[1:-1]
-        self.data = parsed_event_payload(raw_data)
+        self.data = parse_event_payload(raw_data)
 
     def __call__(self, column):
         return self.data[DATA_TYPEMAP[column]]
---
+++ pygerm/tests/test_binarygerm.py
@@ -0,0 +1,121 @@
+import os
+import h5py
+import numpy as np
+import tempfile
+
+from pygerm.zmq import parse_event_payload, DATA_TYPEMAP
+from pygerm.handler import BinaryGeRMHandler
+
+'''
+numpy issue with anding np.uint64 see here:
+    https://github.com/numpy/numpy/issues/2955
+use np.uint64(0x111) typecasting for every unit wise operation
+'''
+
+
+# some setup
+def open_file(fpath):
+    return h5py.File(fpath, 'r')
+
+
+def hdf52event(fpath):
+    ''' requires fpath'''
+    # hdf5 file
+    f = open_file(fpath)
+
+    h = f["GeRM"]
+
+    elem_dict = dict()
+    for key in list(DATA_TYPEMAP):
+        elem_dict[key] = np.array(h[key], dtype=np.uint64)
+
+    return elem_dict
+
+def hdf52germ(fpath):
+    res = hdf52event(fpath)
+    # TODO : make this more formal
+    new_dict = dict()
+    new_dict['chip'] = res['chip']
+    new_dict['chan'] = res['chan']
+    new_dict['td'] = res['timestamp_fine']
+    new_dict['pd'] = res['energy']
+    new_dict['ts'] = res['timestamp_coarse']
+    return payload2germ(**new_dict)
+
+def payload2germ(chip, chan, td, pd, ts):
+    '''
+        Reverse the mapping of GERM data type.
+    '''
+    # uses numpy broadcasting
+    data = np.zeros(len(chip), dtype=np.uint64)
+    data = data | (chip << 27)
+    data = data | (chan << 22)
+    data = data | (td << 12)
+    data = data | pd
+    data = data | ts << 32
+    return data
+
+# two steps:
+'''
+    1. generate the internal data
+    2. generate the whole packet
+
+    TODO : endianness check!
+'''
+
+def generate_germ_data():
+    ''' generate random germ data'''
+    # number of elements
+    N = 3000
+    def generate_data(N):
+        return np.random.randint(low=0, high=1000, size=N, dtype='uint32')
+
+    chip = generate_data(N)
+    chan = generate_data(N)
+    td = generate_data(N)
+    pd = generate_data(N)
+    ts = generate_data(N)
+    germ = payload2germ(chip, chan, td, pd, ts)
+
+    return germ
+
+def generate_germ_packet():
+    germ_data = generate_germ_data()
+
+    germ_binary = np.zeros(len(germ_data)+2, dtype=np.uint64)
+
+    germ_binary[1:-1] = germ_data
+    germ_binary[0] = 0xfeedface00000000
+    germ_binary[-1] = 0xdecafbad
+
+    return germ_binary
+
+
+
+def test_binary_germ():
+    ''' test interchangeability from old hdf5 file format to germ file
+    format.'''
+    # make the tempfile
+    fpath = tempfile.mktemp()
+
+    germ = generate_germ_packet()
+    germ.tofile(fpath)
+    # strip first and last bit of payload
+    # (endianness not issue here since this was generated from memory)
+    chip, chan, td, ps, ts = parse_event_payload(germ[1:-1])
+
+    # instantiate the handler
+    handler = BinaryGeRMHandler(fpath)
+
+    # access some element
+    read_chip = handler('chip')
+    read_chan = handler('chan')
+    read_td = handler('timestamp_fine')
+    read_ps = handler('energy')
+    read_ts = handler('timestamp_coarse')
+
+    assert np.allclose(read_chip, chip)
+    assert np.allclose(read_chan, chan)
+    assert np.allclose(read_td, td)
+    assert np.allclose(read_ps, ps)
+    assert np.allclose(read_ts, ts)
